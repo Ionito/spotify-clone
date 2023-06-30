45bd3eca291403d297e2d750d164438cfc136d54
Ionito <artemarsupial@gmail.com>
2023-06-30 14:58:26 -0300

    feat: added loading and error states

---
+++ app/(site)/error.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Box from "@/components/Box";
+
+const Error = () => {
+  return (
+    <Box className="flex h-full items-center justify-center ">
+      <div>Something went wrong.</div>
+    </Box>
+  );
+};
+
+export default Error;
