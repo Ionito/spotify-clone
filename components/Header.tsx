0588ac0ebc5e3a9da2926a70c81663ff82f46b02
Ionito <artemarsupial@gmail.com>
2023-07-03 09:54:36 -0300

    fix: reset on logout

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,6 +13,7 @@ import useAuthModal from "../hooks/useAuthModal";
 import { useUser } from "../hooks/useUser";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { toast } from "react-hot-toast";
+import usePlayer from "@/hooks/usePlayer";
 
 interface HeaderProps {
   children?: React.ReactNode;
@@ -22,12 +23,14 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({ children, className }) => {
   const router = useRouter();
   const authModal = useAuthModal();
+  const player = usePlayer();
 
   const { user } = useUser();
   const supabaseClient = useSupabaseClient();
 
   const handleLogout = async () => {
     const { error } = await supabaseClient.auth.signOut();
+    player.reset();
     router.refresh();
     if (error) {
       toast.error(error.message);
